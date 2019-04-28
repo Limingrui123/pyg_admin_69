c60e6aa23fd6f23ac1a4de5b8db657503a8a3627
Limingrui <745666914@qq.com>
2019-04-28 20:34:43 +0800

    级联选择器配置完成

--- src/components/goods/paramsMixin.js
+++ src/components/goods/paramsMixin.js
@@ -1,3 +1,27 @@
 export default {
-  name: 'params'
+  name: 'params',
+  data () {
+    return {
+      // 分类级联相关的数据
+      categoryList: [],
+      categoryValues: [],
+      // tabs相关的数据
+      activeName: 'many',
+      disabled: true
+    }
+  },
+  mounted () {
+    this.getData()
+  },
+  methods: {
+    handleChange () {
+    },
+    async getData () {
+      // 获取三级分类数据 且赋值给级联组件
+      const {data: {data, meta}} = await this.$http.get('categories')
+      if (meta.status !== 200) return this.$message.error('获取分类失败')
+      console.log(data)
+      this.categoryList = data
+    }
+  }
 }
