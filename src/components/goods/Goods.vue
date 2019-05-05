3ff3f5ead89eae9f288837deed3b638fe79e5d6a
Limingrui <745666914@qq.com>
2019-05-05 21:19:20 +0800

    商品列表搜索与删除结束

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="good_container">
+  <div class="goods_container">
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>商品管理</el-breadcrumb-item>
@@ -8,22 +8,51 @@
     <el-card>
       <el-row :gutter="20">
         <el-col :span="6">
-            <el-input placeholder="请输入内容">
-              <el-button slot="append" icon="el-icon-search"></el-button>
-            </el-input>
+          <el-input v-model="reqParams.query" placeholder="请输入内容">
+            <el-button @click="search()" slot="append" icon="el-icon-search"></el-button>
+          </el-input>
         </el-col>
         <el-col :span="18">
-          <el-button type="primary" plain>添加用户</el-button>
+          <el-button type="primary"  plain>添加商品</el-button>
         </el-col>
       </el-row>
+      <el-table :data="goodsList">
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="商品名称" width="300px" prop="goods_name"></el-table-column>
+        <el-table-column label="价格" prop="goods_price"></el-table-column>
+        <el-table-column label="重量" prop="goods_weight"></el-table-column>
+        <el-table-column label="创建时间">
+          <template slot-scope="scope">
+            {{scope.row.add_time|ft}}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button icon="el-icon-edit" circle></el-button>
+            <el-button icon="el-icon-delete" @click="delGoods(scope.row.goods_id)" circle></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="pager_container">
+        <el-pagination
+          @current-change="changePager"
+          :page-size="reqParams.pagesize"
+          :current-page="reqParams.pagenum"
+          background
+          layout="prev, pager, next"
+          :total="total">
+        </el-pagination>
+      </div>
     </el-card>
   </div>
 </template>
+
 <script>
-import mixin from './GoodsMixin.js'
+import mixin from './GoodsMixin'
 export default {
   mixins: [mixin]
 }
 </script>
+
 <style scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+import moment from 'moment'
 import './assets/css/global.css'
 // 依赖配置好的axios
 import axios from './http'
@@ -14,6 +15,10 @@ Vue.prototype.$http = axios
 
 // 控制台的日志更详细一些
 Vue.config.productionTip = false
+// 过滤器
+Vue.filter('ft', (v) => {
+  return moment(v * 1000).format('YYYY-MM-DD HH:mm:ss')
+})
 // 入口文件的作用:导入其他依赖
 /* eslint-disable no-new */
 new Vue({
